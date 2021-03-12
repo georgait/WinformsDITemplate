1f5f88de3f2b4c75f38667d80cda81a4e435173b
Georgios Gaitantzis <giorgat88@gmail.com>
2021-03-12 20:39:56 +0200

    Initial commit

---
+++ WinForms/Program.cs
@@ -0,0 +1,73 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    static class Program
+    {
+        public static IHost CurrentHost { get; private set; }
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.File(Path.Combine(Directory.GetCurrentDirectory(), "log.txt"))
+                .CreateLogger();
+
+            Log.Logger.Information("Application Starting");
+
+            CurrentHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+                {
+                    services.AddScoped<EntryForm>();
+                })
+                .UseSerilog()
+                .Build();
+
+            using (var serviceScope = CurrentHost.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                try
+                {
+                    var entryForm = services.GetRequiredService<EntryForm>();
+                    Application.Run(entryForm);
+                    Log.Logger.Information("Application Started");
+                }
+                catch (Exception ex)
+                {
+                    Log.Logger.Error(ex.Message);
+                }
+            }
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
+        }
+    }
+}
